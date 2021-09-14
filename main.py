20037b5c6ecc72180d24ac5830904b76dc02319e
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-09-14 12:53:05 -0700

    more basics

--- main.py
+++ main.py
@@ -52,20 +52,67 @@ else:
 #
 
 # Car Game
-command = ""
-while True:
-    command = input("> ").lower()
-    if command == "start":
-        print("car started...")
-    elif command == "stop":
-        print("car stopped...")
-    elif command == "help":
-        print("""
-start - to start the car
-stop - to stop the car
-quit - to quit
-        """)
-    elif command == "quit":
-        break
-    else:
-        print("sorry, i don't understand you")
+# command = ""
+# started = False
+#
+# while True:
+#     command = input("> ").lower()
+#     if command == "start":
+#         if started:
+#             print("car is already started")
+#         else:
+#             started = True
+#         print("car started...")
+#     elif command == "stop":
+#         if not started:
+#             print("car is already stopped")
+#         else:
+#             started = False
+#         print("car stopped...")
+#     elif command == "help":
+#         print("""
+# start - to start the car
+# stop - to stop the car
+# quit - to quit
+#         """)
+#     elif command == "quit":
+#         break
+#     else:
+#         print("sorry, i don't understand you")
+
+
+
+# for loops
+
+# for item in 'Python':
+#     print(item)
+#
+# for items in range(5, 20, 2):
+#     print(items)
+# total = 0
+# for itemss in [10, 20, 30]:
+#      total += itemss
+# print(f"Total : {total}")
+
+
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+
+
+number = [5, 2, 5, 2, 2]
+for num in number:
+    print("*" * num)
+
+
+# List
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
+names[4] = 'Marry'
+print(names[2:4])
+print(names)
+
+
+large = [2, 4, 6, 8, 9, 10, 30, 50, 90, 49, 44]
+for lg in large:
+    print(lg)
